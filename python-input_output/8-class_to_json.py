1f7a93ca7521ece0721c178703cae363ed5e85c1
esthershimwa <e.shimwa3@alustudent.com>
2025-03-18 05:01:05 -0700

    fixed

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-'''function that returns the dictionary description with simple data structure
-'''
+# function that returns the dictionary description with simple,
+# data structure (list, dictionary, string, integer and boolean),
+# for JSON serialization of an object
+"""
+    define a function 'class_to_json'
+"""
 
 
 def class_to_json(obj):
-    '''module class_to_json
-       returns builds a dictionary
-    '''
+    """
+        return dictionary description for JSON object
+    """
+
     return obj.__dict___
